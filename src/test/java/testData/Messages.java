7bd630d8f28dc0a06fda0d8b08ad8eb24fa27ec3
Gaurav Wardhekara <gauravwardhekar@gmail.com>
2017-11-28 15:01:29 +0530

    Add Card Repository

---
+++ src/test/java/testData/Messages.java
@@ -0,0 +1,112 @@
+package testData;
+
+public class Messages {
+    public static class People{
+        public String Fetch_Off = "Please enable fetch option to load more Abliusers near you.";
+        public String Share_Off = "Your Ablicard is now invisible to other users.";
+        public String No_Cards_fetched = "We do not have Abliusers around you.";
+        public String No_description_on_a_card = "No description on a card";
+        public String On_Card_Distance = "m away approx.";
+        public String Search_Card_Help_Text = "Search user by name/industry";
+        public String Search_Card_Screen_Text = "You can now search any Abliuser here.";
+        public String Card_Page_load_text = "Fetching nearby Ablicards";
+        public String Fetch_Off_Screen_Text = "Please enable fetch option to load more Abliusers near you.";
+        public String No_Internet = "Please check your internet connection.";
+        public String Last_Page_on_Cards = "No more Abliusers near you.";
+        public String Posts_Default_Screen_Text = "Relevant to you industry and city.";
+        public String Last_page_on_Posts = "No more posts available.";
+        public String No_Posts_found = "No posts available. Please try by changing filters.";
+        public String No_of_remaining_posts_Part_One = "You are left with";
+        public String No_of_remaining_posts_Part_Two = "posts for this month.";
+        public String Click_on_Im_Interested = "Show interest via";
+        public String Post_Remove_from_Favourites = "Do you really want to remove this post from favourites?";
+        public String Post_withdraw_interest = "Do you really want to withdraw your interest from this post?";
+        public String Click_On_Offering_Click_On_Requirment = "There must be at least one of these two filters.";
+        public String Pin_Request = "Would you like to Request for PIN to view this card?";
+    }
+
+    public static class People_Cards_Filter{
+        public String No_Cards_Found_For_Applied_Filter = "We do not have Abliusers for applied filters.";
+        public String Click_on_Filter_when_there_are_no_nearby_cards = "No data to filter";
+        public String Clear_Filter_on_Cards_Posts = "Do you want to remove all the filters applied?";
+        public String Press_Close_Or_Back_Button = "Some changes have been made to the filter options.";
+    }
+
+    public static class People_Post_Filter{
+        public String No_Indutry = "Please select at least one industry to apply filter on posts.";
+        public String No_City = "Please select at least one city to apply filter on posts.";
+        public String Clear_Industries = "Are you sure you want to remove all selected industries?";
+        public String Clear_Cities = "Are you sure you want to remove all selected cities?";
+        public String Clear_city_and_industry_and_then_click_done = "Please select at least one city and one industry to apply filter on posts.";
+        public String Delete_Post = "Post does not exists anymore.";
+    }
+
+    public static class Explore{
+        public String Regiter_Now = "You have successfully registered to this event.";
+        public String Confirm_Reiteration = "Would you like to confirm your registration for this event?";
+        public String Unfollow = "By unfollowing, you won't be able to receive any feeds from this organisation.";
+        public String Private_User  = "This user's profile is private.";
+        public String No_Cards = "This user does not have any card.";
+        public String Search_Organisations = "Search 50+ organisations";
+        public String Search_Network = "Search by name";
+        public String Search_Followed_Organisations = "Search by organisation(s)";
+        public String No_Followed_Organisations = "Please follow organisations to get event updates.";
+        public String No_My_Networks = "Create your Ablicard to be a member of Ablifree Networks.";
+    }
+
+    public static class Favourites{
+        public String No_Posts = "No more posts available.";
+        public String No_Favourite_cards = "No favourite Ablicards.";
+        public String Search_Card = "Search user by name";
+        public String No_Cards_Click_On_filter = "You don't have any favourite Ablicards for applied filter";
+        public String Delete_Card = "Are you sure you want to delete this Ablicard?";
+        public String Remove_post_form_Favourites = "Do you really want to remove this post from favourites?";
+    }
+
+    public static class Profile{
+        public String No_My_Posts = "You do not have any posts added.";
+        public String No_My_Events = "Events are not available.";
+        public String No_Followed_Organisations = "Please follow organisations to get event updates.";
+        public String No_Views_On_card = "No views for this card yet.";
+        public String No_Favourite_Card = "No favourites for this card yet.";
+        public String New_Card = "No Ablicard added. Please add your first Ablicard.";
+        public String No_Cards = "You do not have any posts added.";
+        public String Email_Verification = "Your email address is not verified. We will send you verification link on your register email address";
+        public String Resend_Verification_Email = "Verification email is successfully sent to your registered email address.";
+        public String Security_Pin = "Security pin is used to unlock Ablicards which have document visibility off. Your security pin has been copied to clipboard.";
+    }
+
+    public static class Card_Details{
+        public String Click_On_favprite = "Card is favourited successfully for offline usage. You can access this card from favourites.";
+    }
+
+    public static class Add_Edit_card{
+        public String Click_Submit_No_Information = "Please add Ablicard photos or choose template.";
+        public String Add_first_Image = "Do you want to add back photo of the Ablicard?";
+        public String Add_Second_Image = "Ablicard back image successfully added. Go to step 2?";
+        public String Invalide_Email = "Please enter valid email address.";
+        public String No_Industry_selected = "Please select your industry.";
+        public String Invalid_Company_Name = "Please enter valid company name.";
+        public String Invalid_Field_Of_Work = "Please enter valid field of work.";
+        public String Invalid_Landline = "Please enter valid business landline number.";
+        public String Invalid_Mobile = "Please enter valid mobile number.";
+        public String Invalid_Website = "Please enter valid website details.";
+        public String Invalid_Designation = "Please enter valid designation.";
+        public String Invalid_Business_Description = "Please enter valid business description.";
+        public String Document_Visibility_On = "Your Ablicard is public and can be seen by everyone around you.";
+        public String Document_Visibility_Off = "Your Ablicard is private and can be shared only by using a PIN assigned to you in your profile.";
+        public String Delete_Card = "Are you sure you want to delete this card?";
+        public String Discard_Edit = "This will discard all your changes. Do you want to go back?";
+        public String Delete_card_Image = "Are you sure you want to remove this photo?";
+        public String Step_Two = "All the details will be shown to other users around you.";
+    }
+
+    public static class Profile_Card_details{
+        public String No_Views = "No views for this card yet.";
+        public String No_Favourites = "No favourited for this card yet.";
+        public String No_Business_Description = "Add your business description.";
+        public String Profile_Percentage = "For 100% completion of profile : Fill all the fields verify email should have one card.";
+        public String Card_Percentage = "For 100% completion of card : Fill all the fields verify email should have one card.";
+        public String Visible_Invisible = "Unable to update document visibility.";
+    }
+}
